2944206c83d7a34541bee42847052cb19a190609
sreeya-24 <70120486+sreeya-24@users.noreply.github.com>
2021-01-23 10:28:28 -0800

    Add files via upload

---
+++ MatterJSBoilerPlate-master/sketch.js
@@ -0,0 +1,51 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var options={
+	isStatic : false,
+	restitution:0.3,
+	friction:0.5,
+	density:1.2
+
+}
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+if(keyCode===UP_ARROW){
+	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+
+}
+
+}
+
+
+
